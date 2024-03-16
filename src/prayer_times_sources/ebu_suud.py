abea29df129f5cb5e5246386c610eefe2a098403
muhammedfatih <mfatihinanc@gmail.com>
2024-03-16 22:01:53 +0000

    update for ebusuud headers

--- src/prayer_times_sources/ebu_suud.py
+++ src/prayer_times_sources/ebu_suud.py
@@ -9,7 +9,16 @@ class EbuSuud:
         self.times=['', '', '', '', '', '']
         # Send a GET request to the webpage and retrieve the source code
         url = "https://namazvakitleri.diyanet.gov.tr/tr-TR/14096/londra-icin-namaz-vakti"
-        response = requests.get(url)
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "keep-alive",
+        }
+
+        response = requests.get(url, headers=headers)
         log(f"EbuSuud sent the request and its response ({response.status_code}).")
 
         # Check if the request was successful (status code 200)
